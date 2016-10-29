5fd9fde00a9c579956d7069dbd94681212a00dc3
D3mokrator <badday2u@gmail.com>
2016-10-29 22:46:04 +0200

    upgraded player display: overlapping players

--- b_screen.py
+++ b_screen.py
@@ -1,7 +1,8 @@
 import pygame
-
-screen = pygame.display.set_mode((1000,1000))
-background = pygame.image.load("data/background.jpg").convert()
+from pygame.locals import *
+Y_Size_Const = 80
+X_Size_Const = 60
+action_queue = []
 
 class Player():
 	def __init__(self, image, position):
@@ -11,38 +12,74 @@ class Player():
 	def moveto(self, x, y):
 		self.position[0]=x
 		self.position[1]=y
-		screen.blit(self.image,(x,y))
+		objects.sort(key = lambda x: x.position[1])
 		for o in objects:
-			if o != self :
-				screen.blit(o.image, (o.position[0], o.position[1]))
-			else:
-				pass
+			screen.blit(o.image, (o.position[0]*X_Size_Const, o.position[1]*Y_Size_Const))
 		pygame.display.update()
 		screen.blit(background, (0,0))
 
 	def moveby(self, x, y):
 		self.position[0]+=x
 		self.position[1]+=y
-		screen.blit(self.image,(self.position[0],self.position[1]))
-		for o in objects:
-			if o != self :
-				screen.blit(o.image, (o.position[0], o.position[1]))
-			else:
-				pass
-		pygame.display.update()
-		screen.blit(background, (0,0))
+		self.moveto(self.position[0],self.position[1])
 
-objects = []
-objects.append(Player(pygame.image.load("data/player.png").convert(), [0,0]))
-screen.blit(background, (0,0))
-pygame.display.update()
+def Screen_init(size_x,size_y):
+	global screen
+	screen = pygame.display.set_mode((size_x,size_y))
 
-def animate(player):
-	for x in range(50):
-		for y in range(50):
-			player.move(x*20,y*20)
-			pygame.time.delay(50)
+def set_background(filename):
+	screen.fill((0,0,0))
+	global background
+	background = pygame.image.load("data/"+filename).convert()
+	screen.blit(background, (0,0))
 	pygame.display.update()
 
+
+def Objects_init(number):
+	global objects
+	objects = []
+	for i in range(number):
+		objects.append(Player(pygame.image.load("data/player.png").convert(), [0,0]))	# will probably want to load some different images
+	
+def handle(p):
+	clock = pygame.time.Clock()	# should probably be in a higher instance
+	while(1):
+		for event in pygame.event.get():
+			print ("Its something")		# debug really
+			if event.type == QUIT:
+				return
+			elif event.type == KEYDOWN and event.key == K_ESCAPE:
+				return True
+			elif event.type == KEYDOWN and event.key == K_UP:
+				p.moveby(0,-1)
+			elif event.type == KEYDOWN and event.key == K_DOWN:
+				p.moveby(0,1)
+			elif event.type == KEYDOWN and event.key == K_LEFT:
+				p.moveby(-1,0)
+			elif event.type == KEYDOWN and event.key == K_RIGHT:
+				p.moveby(1,0)
+			elif event.type == KEYDOWN and event.key == K_RETURN:
+				return False
+	clock.tick(60)
+
+def test_handling():
+	action_queue.append(objects[0])
+	action_queue.append(objects[1])
+	action_queue.append(objects[2])
+	while(action_queue):
+		if(handle(action_queue[0])):
+			break
+		action_queue.append(action_queue[0])
+		action_queue.pop(0)
+	action_queue.clear()
+
+pygame.init()
+if not pygame.key:	#	debug; not really important
+	print ("Keyboard is fucked")
+if not pygame.event:
+	print ("Event is fucked")
+Screen_init(1000,1000)	# arbitrarily decide what your game screen should look like, you chauvinist
+set_background("background.jpg")
+Objects_init(3)
 
 print ("Succesfully imported module \"b_screen\"")
